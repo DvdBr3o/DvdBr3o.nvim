671d68423db6f1b2fdd7ca18319b36d668163116
DvdBr3o <dvdbr3o@qq.com>
2024-02-09 17:05:33 +0800

    [New] Added cppman for cppreference in-vim, not available so far though.

---
+++ lua/plugins/lang-cpp.lua
@@ -0,0 +1,7 @@
+return {
+	-- {
+	-- 	"v1nh1shungry/cppman.nvim",
+	-- 	dependencies = "nvim-telescope/telescope.nvim", -- optional, if absent `vim.ui.select()` will be used
+	-- 	opts = {},
+	-- },
+}
