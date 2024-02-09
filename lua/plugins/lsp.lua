d408d63f879d83b9b1dcd84a01a2956dc129e26d
DvdBr3o <dvdbr3o@qq.com>
2024-02-09 17:02:47 +0800

    [Update] Added common used language servers.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,13 @@
 return {
-    { import = "lazyvim.plugins.extras.lang.clangd" },
-    { import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.clangd" },
+	{ import = "lazyvim.plugins.extras.lang.json" },
+	{ import = "lazyvim.plugins.extras.lang.typescript" },
+	{ import = "lazyvim.plugins.extras.lang.tailwind" },
+	{
+		"Mythos-404/xmake.nvim",
+		lazy = true,
+		event = "BufReadPost xmake.lua",
+		config = true,
+		dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+	},
 }
