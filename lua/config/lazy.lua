f57bfe2a0b1116f364f03568f00f4f20b4790fa9
DvdBr3o <dvdbr3o@qq.com>
2026-03-04 17:02:22 +0800

    fix cpp support.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.ui.edgy" },
     -- import/override with your plugins
     { import = "plugins" },
